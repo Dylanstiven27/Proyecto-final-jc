688bf44bcd123e5d7b06a389b49d7b3c2f0978ed
Dylanstiven27 <dilanstivenmuozsernad@gmail.com>
2022-12-01 15:10:14 -0500

    Se agrego preguntas linkeado con js

--- js/pregunta.js
+++ js/pregunta.js
@@ -1,4 +1,17 @@
+escogerpregunta(0)
 
-function funcion(){
-    location.href = "../html/pregunta2.html";
+let base_preguntas = readText("base de preguntas.json")
+let interprete_bp = JSON.parse(base_de_base_preguntas)
+
+function escogerpreguntaAleatoria(){
+    escogerpregunta(Math.floor(Math.random()*interprete_bp.lenght))
+}
+
+function escogerpregunta(){
+    pregunta = interprete_bp[n]
+    select_id("opcion 1").innerHTML = pregunta.respuesta
+    select_id("opcion 2").innerHTML = pregunta.incorrecta1
+    select_id("opcion 3").innerHTML = pregunta.incorrecta2
+    select_id("opcion 4").innerHTML = pregunta.incorrecta3
+    select_id("opcion 5").innerHTML = pregunta.incorrecta4
 }
