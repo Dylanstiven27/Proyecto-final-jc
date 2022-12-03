70768813197267bc827383b41f5d2720ee9faaf7
Dylanstiven27 <dilanstivenmuozsernad@gmail.com>
2022-12-03 02:15:14 -0500

    se agregaron sus funcionalidades y se creo otro archivo js

---
+++ js/cambiodepreguntas.js
@@ -0,0 +1,39 @@
+function funcion() {
+    location.href = "../html/pregunta2.html";
+}
+
+function funcion2() {
+    location.href = "../html/pregunta3.html";
+}
+
+function funcion3() {
+    location.href = "../html/pregunta4.html";
+}
+
+function funcion4() {
+    location.href = "../html/pregunta5.html";
+}
+
+function funcion5() {
+    location.href = "../html/pregunta6.html";
+}
+
+function funcion6() {
+    location.href = "../html/pregunta7.html";
+}
+
+function funcion7() {
+    location.href = "../html/pregunta8.html";
+}
+
+function funcion8() {
+    location.href = "../html/pregunta9.html";
+}
+
+function funcion9() {
+    location.href = "../html/pregunta10.html";
+}
+
+function funcion10() {
+    location.href = "../html/finaldeljuego.html";
+}
